613c633fb8e0ae3dc8f0565e3b6b01ab786189ab
Jacob Kessler <jacob.kessler@case.edu>
2020-07-11 12:28:06 -0500

    Initial Commit

---
+++ ProximitySearch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProximitySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
